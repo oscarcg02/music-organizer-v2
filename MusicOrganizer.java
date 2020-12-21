e7c1f3f5554e10c9f85ac9f0a37f805e8aa13b02
Oscarcg02 <oscarcg02@gmail.com>
2020-12-21 11:53:39 +0100

    04 mensaje de error sin repeticion

--- MusicOrganizer.java
+++ MusicOrganizer.java
@@ -13,7 +13,7 @@ public class MusicOrganizer
     private ArrayList<String> files;
     // A player for the music files.
     private MusicPlayer player;
-        
+
     /**
      * Create a MusicOrganizer
      */
@@ -22,7 +22,7 @@ public class MusicOrganizer
         files = new ArrayList<String>();
         player = new MusicPlayer();
     }
-    
+
     /**
      * Add a file to the collection.
      * @param filename The file to be added.
@@ -31,7 +31,7 @@ public class MusicOrganizer
     {
         files.add(filename);
     }
-    
+
     /**
      * Return the number of files in the collection.
      * @return The number of files in the collection.
@@ -40,7 +40,7 @@ public class MusicOrganizer
     {
         return files.size();
     }
-    
+
     /**
      * List a file from the collection.
      * @param index The index of the file to be listed.
@@ -52,7 +52,7 @@ public class MusicOrganizer
             System.out.println(filename);
         }
     }
-    
+
     /**
      * Remove a file from the collection.
      * @param index The index of the file to be removed.
@@ -63,7 +63,7 @@ public class MusicOrganizer
             files.remove(index);
         }
     }
-    
+
     /**
      * Start playing a file in the collection.
      * Use stopPlaying() to stop it playing.
@@ -82,19 +82,22 @@ public class MusicOrganizer
     {
         player.stop();
     }
-    
+
     public void listAllFiles() {
-       int posicion = 0;
-       for (String filename : files) {
-           posicion = posicion + 1;
-           System.out.println(posicion  + "." + filename);
+        int posicion = 0;
+        for (String filename : files) {
+            posicion = posicion + 1;
+            System.out.println(posicion  + "." + filename);
         }
     }
+
     public void listMatching (String searchString) {
         for (String filename : files) {
             if (filename.contains(searchString)) {
                 System.out.println(filename);
             }
         }
+        System.out.println("Error, no hay ninguna cancion con esas caracterisiticas");
+            
     }
 }
